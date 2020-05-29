dbd8cd9f1a2a88e08bbf94fb001f6c8acf37a0fc
asamacode <trhuuluong@gmail.com>
2020-05-29 07:14:46 +0700

    init project

---
+++ src/test/java/com/asama/onlinequiz/OnlinequizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asama.onlinequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinequizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
